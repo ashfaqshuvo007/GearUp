e8bddf62b2c58cbb54e400f368208e7fdec5cf06
Ashfaq Hussain Ahmed <ashfaqshuvo14@gmail.com>
2026-07-30 17:42:36 +0600

    Fixed typo

--- src/utils/encrypt.ts
+++ src/utils/encrypt.ts
@@ -18,7 +18,7 @@ export const verifyToken = async (token: string, secret: string) => {
   try {
     const verifiedToken = jwt.verify(token, secret);
     return {
-      sucess: true,
+      success: true,
       data: verifiedToken,
     };
   } catch (error) {
